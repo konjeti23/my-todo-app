2bc40b80cfb5ef8f149c78070ae112c1bc402c12
Nithin <konjetinithin2311@gmail.com>
2025-06-25 21:51:51 -0500

    Initial commit

---
+++ functions.py
@@ -0,0 +1,20 @@
+FILEPATH = "todos.txt"
+
+
+
+def get_todos(filepath = FILEPATH):  # filepath = parameter
+    with open(filepath, "r") as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath = FILEPATH):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+
+
+
+if __name__ == "__main__":
+    print("hello from functions")
